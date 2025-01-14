724ddae85d3494d544c27efb4e5bfdc77f421834
haldaniko <haldaniko@gmail.com>
2025-01-14 22:41:03 +0100

    add 'track_package_view'

--- tracking/views.py
+++ tracking/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .helpers import create_tracker, track_package
 
-# Create your views here.
+
+def track_package_view(request, tracking_id):
+    try:
+        result = track_package(tracking_id)
+
+        if result["success"]:
+            return JsonResponse(result["data"], safe=False)
+
+        elif (
+            result["status_code"] == 404
+            and any(error.get("code") == "tracker_not_found" for error in result["error"].get("errors", []))
+        ):
+            create_tracker(tracking_id)
+            result = track_package(tracking_id)
+
+        return JsonResponse(result["data"], safe=False)
+
+    except ValueError as e:
+        return JsonResponse({"error": f"Value Error: {str(e)}"}, status=400)
+
+    except KeyError as e:
+        return JsonResponse({"error": f"Key Error: {str(e)}"}, status=400)
+
+    except Exception as e:
+        return JsonResponse({"error": f"Unexpected Error: {str(e)}"}, status=500)
